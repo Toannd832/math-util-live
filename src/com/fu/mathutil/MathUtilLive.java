761e8ecc604251e653c9dfea4ec79abfdae2c02c
Toannd832 <toan03182@gmail.com>
2023-01-05 16:35:02 +0700

    Lan thu 2 lam chuyen ay - quen roi

--- src/com/fu/mathutil/MathUtilLive.java
+++ src/com/fu/mathutil/MathUtilLive.java
@@ -5,8 +5,6 @@
  */
 package com.fu.mathutil;
 
-
-
 /**
  *
  * @author toan0
@@ -17,11 +15,16 @@ public class MathUtilLive {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-      
+
         long result = MathUtil.getfactorial(5);
         System.out.println("Expect 5!= 120; actual: " + result);
         System.out.println(" Expect 6!= 720; actual: " + MathUtil.getfactorial(6));
         System.out.println(" Expect 0!= 1; actual: " + MathUtil.getfactorial(0));
+        
+        //THÊM CODE SAU LẦN ĐẦU TIÊN LÀM CHUYỆN ẤY-CODE LÊN SERVER 4:32PM 4/1/2023
+        System.out.println(" Expect 1!= 1; actual: " + MathUtil.getfactorial(1));
+        System.out.println(" Expect 3!= 6; actual: " + MathUtil.getfactorial(3));
+
     }
-    
+
 }
